c2824c87a806040f59dead29bcda47356fc94534
Sammy Muchiri <dev.miano@gmail.com>
2022-04-30 01:05:42 +0300

    added sass to the project

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
+from warnings import filters
 from flask import Flask
+from flask_assets import Environment, Bundle
 from config import config_options
 
 
 def create_app(config_name):
-  app = Flask(__name__, static_folder='assets')
+  app = Flask(__name__)
   app.config.from_object(config_options[config_name])
   
   from .main import main as main_blueprint
@@ -11,5 +13,10 @@ def create_app(config_name):
   
   from .requests import configure_request
   configure_request(app)
+  
+  assets = Environment(app)
+  sass = Bundle('sass/global.scss', filters='pyscss, cssmin', output='css/global.min.css')
+  assets.register('sass_all', sass)
+  sass.build()
   
   return app
